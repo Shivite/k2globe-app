5c81e59ca4ae74609b7d3b2328f95089c843794a
Ravinder Kr. Sharma <ravinder.acs@gmail.com>
2024-07-23 21:41:45 +0530

    notfound and not found function addon

---
+++ src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
@@ -0,0 +1,5 @@
+export default function NotFound(){
+    return <>
+        <h1>route not  not found</h1>
+    </>
+}
--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,7 +1,12 @@
+import { notFound } from "next/navigation"
 export default function ReviewListing ({params}:{params:{productId:String, reviewId: String}}) {
     console.log("params---", params)
+    if(parseInt(params?.reviewId) > 100){
+        notFound();
+    }
     return (
         <>
+        
         product {params.productId} - review - {params.reviewId}
         </>
     )
